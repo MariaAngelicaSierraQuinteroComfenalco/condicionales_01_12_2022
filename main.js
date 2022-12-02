f92b338b6cd626623b02c0e7a31ed60510166d3c
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-12-02 16:07:37 -0500

    if simple

---
+++ main.js
@@ -0,0 +1,17 @@
+addEventListener("DOMContentLoaded", (e) => {
+//if simple es una estructura de tomar decisiones 
+//estructura
+// if (expresión) { si se cumple la condicion pasara hacer las acciones si no hay termina
+//    }
+//ademas se puede escribir de esta forma 
+//   if (llueve)
+//     alert("Cae agua");
+//al igual que esta 
+//  if (llueve) alert("Cae agua");
+ 
+//ejemplo
+if (dia == "lunes") //decimos si la variable dia es igual a lunes
+document.write ("Que tengas un feliz comienzo de semana")//accion a realizar 
+
+
+})
