78d500a98bad6ddb04fac9d14719bfcbb49fc534
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-12-02 18:43:50 -0500

    operador if

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 addEventListener("DOMContentLoaded", (e) => {
-//if-else en cuando al tomar decisiones nos devulve un valor negtaivo 
-//es uno de los mas utilizados
-//if (expresión) { 
-    //acciones a realizar en caso positivo 
- //} else { 
-    //acciones a realizar en caso negativo 
- //}
-
+//operador if es una forma mas esquematizacion de if sencillos
+// Variable = (condición) ? valor1 : valor2
+//es la estructura esta tambein se asigna un valor en una variable
 //ejemplo
 
-if (credito >= precio) { //pregunta la condicion y si es positivo se ejecuta
-    document.write("has comprado el artículo " + nuevoArtículo) //enseño compra 
-    carrito += nuevoArticulo 
-    credito -= precio 
- } else { //si es negativa se realiza otro procedimiento
-    document.write("se te ha acabado el crédito")  
-    window.location = "carritodelacompra.html" 	
- }
+momento = (hora_actual < 12) ? "Antes del mediodía" : "Después del mediodía"
+
+
 })
