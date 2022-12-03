52bf7060223a153aecf3ad157f9e389bf551ce42
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-12-02 19:02:09 -0500

    switch

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 addEventListener("DOMContentLoaded", (e) => {
-// IF anidados
-//es hacer un if dentro de otro if y asi sucecivamente para muchas situciones es mas efectiva
-//ejemplo
-var numero1=23 //variables
-var numero2=63 
-if (numero1 == numero2){ //primer if
-   document.write("Los dos números son iguales") 
-}else{ //primer if-else
-   if (numero1 > numero2) { //segundo if 
-      document.write("El primer número es mayor que el segundo") 
-   }else{ //segundo if-else
-      document.write("El primer número es menor que el segundo") 
-   } 
-}
+// switch
+// evalua los diferentes casos y ejercuta las que sean positiva 
+//estructura 
+//switch (expresión) {
+//  case valor1:
+//    //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+//    [break;]
+//  case valor2:
+//     //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
+//     [break;]
+switch (expr) {
+    case 'Naranjas':
+      console.log('El kilogramo de naranjas cuesta $0.59.');
+      break;//si no se pone seguira ejecutandose todo sin parar, a diferencia que si esta no debera 
+    case 'Manzanas'://cada caso 
+      console.log('El kilogramo de manzanas cuesta $0.32.');//el parametro
+      break;
+    case 'Platanos':
+      console.log('El kilogramo de platanos cuesta $0.48.');
+      break;
+    case 'Cerezas':
+      console.log('El kilogramo de cerezas cuesta $3.00.');
+      break;
+    case 'Papayas':
+      console.log('El kilogramo de mangos y papayas cuesta $2.79.');
+      break;
+    default: // si ninguno de los caso se encontro concidencia entonces el default nos lo dira al final
+      console.log('Lo lamentamos, por el momento no disponemos de ' + expr + '.');
+  }
+  
+  console.log("¿Hay algo más que te quisiera consultar?");
 
 })
