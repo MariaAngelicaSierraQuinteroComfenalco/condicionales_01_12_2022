91e77ea137f41e461200a8411ca1e6065ad2ca1a
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-12-02 19:08:25 -0500

    switch multiples casos

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
 addEventListener("DOMContentLoaded", (e) => {
-// switch
-// evalua los diferentes casos y ejercuta las que sean positiva 
-//estructura 
+// switch Operación única con múltiples casos
+// si no hay un break continuara la ejecucion del siguiente case
+//estructura es la misma sino sin break en algunos casos 
 //switch (expresión) {
 //  case valor1:
 //    //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
-//    [break;]
 //  case valor2:
 //     //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
 //     [break;]
-switch (expr) {
-    case 'Naranjas':
-      console.log('El kilogramo de naranjas cuesta $0.59.');
-      break;//si no se pone seguira ejecutandose todo sin parar, a diferencia que si esta no debera 
-    case 'Manzanas'://cada caso 
-      console.log('El kilogramo de manzanas cuesta $0.32.');//el parametro
-      break;
-    case 'Platanos':
-      console.log('El kilogramo de platanos cuesta $0.48.');
-      break;
-    case 'Cerezas':
-      console.log('El kilogramo de cerezas cuesta $3.00.');
-      break;
-    case 'Papayas':
-      console.log('El kilogramo de mangos y papayas cuesta $2.79.');
-      break;
-    default: // si ninguno de los caso se encontro concidencia entonces el default nos lo dira al final
-      console.log('Lo lamentamos, por el momento no disponemos de ' + expr + '.');
-  }
-  
-  console.log("¿Hay algo más que te quisiera consultar?");
+
+var Animal = 'Jirafa';
+switch (Animal) {
+  case 'Vaca':// se comportan de la misma manera 
+  case 'Jirafa':
+  case 'Perro':
+  case 'Cerdo'://se comporta diferente por eso tiene el  break
+    console.log('Este animal subirá al Arca de Noé.');
+    break;
+  case 'Dinosaurio':
+  default:
+    console.log('Este animal no lo hará.');
+}
+
+//Operaciones encadenadas con múltiples casos
+// dependiendo del valor entero dado, se pueden recibir diferentes resultados
 
 })
